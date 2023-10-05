cfe677ae57b52da0e6d577344db56f6f31bf0543
Smarika Thapa <131597379+smarrrr@users.noreply.github.com>
2023-10-05 12:14:55 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function calculateResult() {
+    var result = document.getElementById('result');
+    var expression = result.textContent;
+    var answer = eval(expression);
+    result.textContent = answer;
+}
+
+function deleteLastCharacter() {
+    var result = document.getElementById('result');
+    result.textContent = result.textContent.slice(0, -1);
+}
+
+function clearResult() {
+    var result = document.getElementById('result');
+    result.textContent = '0';
+}
 
